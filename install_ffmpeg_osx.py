9ead2905d3c38aafd0bbb2e1fcfa8d634e9aa003
Ken Bailey <kgbailey92@gmail.com>
2018-03-28 13:39:33 -0500

    Added notes

--- install_ffmpeg_osx.py
+++ install_ffmpeg_osx.py
@@ -1,3 +1,15 @@
+'''
+    Kenneth Bailey
+    3/17/18
+
+    Usage --> "python3 install_ffmpeg_osx.py"
+
+    Notes:
+        - Run before first run of any scripts if you are on a mac. 
+        - Installs necessary files required to transcode video
+
+'''
+
 import imageio
 imageio.plugins.ffmpeg.download()
 from moviepy.editor import *
