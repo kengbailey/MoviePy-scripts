67d0bb74e6531f1b5582bbc25d81265195488b0c
Ken Bailey <kgbailey92@gmail.com>
2018-03-28 13:36:28 -0500

    Renamed methods to new naming convention; Added .gitignore

--- concat.py
+++ concat.py
@@ -1,10 +1,27 @@
+'''
+    Kenneth Bailey
+    3/17/18
+
+    Usage -->  "python3 concat.py <outfile.name>"
+
+    Notes:
+        - Currently concats all videos located in directory in which the script is ran. 
+        - Concats the video in alphanumeric order
+
+    To Do:
+        - Implement exception handling
+        - Test with more video types(.avi, .mov, etc)
+        - Prompt user to select order in which videos will be concatenated
+
+'''
+
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import sys
 import os
 
 FORMATS = [".MP4", ".mp4"]
 
-def concat_clips(video_list):
+def concatClips(video_list):
     clip_list = []
     for video in video_list:
         clip = VideoFileClip(video)
@@ -13,33 +30,36 @@ def concat_clips(video_list):
     final_clip.write_videofile(sys.argv[1])
     
 
-def get_video_files():
+def getVideoFiles():
     video_files = []
     files_path = os.listdir()    
     for path in files_path:
-        if is_video(path):
+        if isVideo(path):
             video_files.append(os.getcwd()+'/'+path)
     return video_files
 
-def is_video(vid):
+def isVideo(vid):
     if any(x in vid for x in FORMATS):
         return True
     else:
         return False
 
 if __name__ == '__main__':
+    
     # check for video name argument
     if len(sys.argv) == 1:
         print("Failed to input video out filename!")
         print("Usage: python3 concat.py out.mp4")
         sys.exit(0)
-    if not is_video(sys.argv[1]):
+    
+    # check for proper output type
+    if not isVideo(sys.argv[1]):
         print("Output file not valid!")
         print("Usage: python3 concat.py out.mp4")
         sys.exit(0)
 
     # compile video list
-    video_list = get_video_files()
+    video_list = getVideoFiles()
 
     # concatenate videos
-    concat_clips(video_list)
+    concatClips(video_list)
